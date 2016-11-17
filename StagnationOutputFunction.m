367c09afab217d1a1fa5eb811f7f2700bb2c65ca
dflowers7 <dflowers@mit.edu>
2016-11-17 11:08:58 -0500

    Fixed numerous bugs. Seems to work now.

--- StagnationOutputFunction.m
+++ StagnationOutputFunction.m
@@ -1,15 +1,38 @@
 function fun = StagnationOutputFunction(relTol)
 
 lastfval = [];
+slowstepcount = [];
 fun = @outputfun;
 
     function stop = outputfun(x, optimValues, state)
-        fval = optimValues.fval;
-        if ~isempty(lastfval)
-            deltaG = lastfval - fval;
-            stop = deltaG/lastfval < relTol;
+        stop = false;
+        switch state
+            case 'init'
+                % Reset lastfval at the beginning of the optimization
+                lastfval = [];
+                slowstepcount = 0;
+            case 'iter'
+                if isfield(optimValues, 'fval')
+                    % fmincon case
+                    fval = optimValues.fval;
+                else
+                    % lsqnonlin case
+                    fval = optimValues.resnorm;
+                end
+                if ~isempty(lastfval)
+                    deltaG = lastfval - fval;
+                    isslowstep = deltaG/lastfval < relTol;
+                    if isslowstep
+                        slowstepcount = slowstepcount + 1;
+                    else
+                        slowstepcount = 0;
+                    end
+                end
+                if slowstepcount >= 5
+                    stop = true;
+                end
+                lastfval = fval;
         end
-        lastfval = fval;
     end
 
 end
